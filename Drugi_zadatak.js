b933394d5d505500ed5958b6b3c6d05f45d7ad14
resadsp <resadspahovic94@gmail.com>
2023-08-31 17:31:44 +0200

    Second Commit

--- Drugi_zadatak.js
+++ Drugi_zadatak.js
@@ -1,1 +1,8 @@
-console.log ("neki tekst za drugi zadatak");
+
+for(let i=1; i<=5; i++){
+    console.log(" ".repeat(5-i)+("*").repeat(2*i-1));
+}
+for(let j=4; j>=1; j--){
+    console.log((" ").repeat(5-j)+"*".repeat(2*j-1));
+}
+
